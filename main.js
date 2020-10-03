c59bf56bd217d97ada226c2caa7785a9c8559c59
inezabonte <inezabonte@gmail.com>
2020-10-03 20:03:09 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const mymap = L.map("mapid").setView([-1.9403, 29.8739], 9);
+
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
