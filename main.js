d237e9855a842342c2a1a6470916709cfa39727b
inezabonte <inezabonte@gmail.com>
2020-10-04 21:30:10 +0200

    added intro page

--- main.js
+++ main.js
@@ -57,6 +57,55 @@ function style(density) {
   };
 }
 
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: "#666",
+    dashArray: "",
+    fillOpacity: 0.7,
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+  info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+
+  info.update();
+}
+
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+var info = L.control();
+
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create("div", "info"); // create a div with a class "info"
+  this.update();
+  return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+  this._div.innerHTML =
+    "<h4>US Population Density</h4>" +
+    (props
+      ? "<b>" +
+        props.name +
+        "</b><br />" +
+        props.density +
+        " people / mi<sup>2</sup>"
+      : "Hover over a state");
+};
+
+info.addTo(mymap);
+
 $.getJSON(
   "https://raw.githubusercontent.com/inezabonte/flood-detection/main/RW_districts.geojson",
   function (data) {
